1203a28d3269ab00ed113b3722d98f180a63f699
SubwayMan <tyche4490@gmail.com>
2021-10-02 22:00:04 -0700

    added basic bot file

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from dotenv import load_dotenv
+import os
+import time
+
+load_dotenv()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(os.environ.get("bot-token"))
